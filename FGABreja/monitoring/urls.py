c328209f0f349158b35819aed8db18cc10c1c5b0
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-12-05 05:13:13 -0200

    Refactoring pages
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>
    Signed-off-by: Macario Soares <macario.junior@gmail.com>

--- FGABreja/monitoring/urls.py
+++ FGABreja/monitoring/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
-from monitoring.views import get_temperature_average, HomeView
+from monitoring.views import (get_temperature_average, HomeView,
+                              NewProcessView, MonitoringView)
 
 
 urlpatterns = [
     url(r'^temperature/average/$', get_temperature_average,
         name='temperature_average'),
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^monitoring/$', MonitoringView.as_view(), name='monitoring'),
+    url(r'^process/new$', NewProcessView.as_view(), name='new_process'),
 ]
--- FGABreja/monitoring/views.py
+++ FGABreja/monitoring/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
-from monitoring.models import ThermalSensor
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from defaults.views import FGABrejaView
@@ -16,7 +15,19 @@ def get_temperature_average(request):
 class HomeView(FGABrejaView):
 
     def get(self, request):
-        sensor_data = ThermalSensor.objects.filter(
-            identifier=1).order_by('date', 'time')
         return render_to_response('home.html', locals(),
+                                  context_instance=RequestContext(request))
+
+
+class MonitoringView(FGABrejaView):
+
+    def get(self, request):
+        return render_to_response('monitoring.html', locals(),
+                                  context_instance=RequestContext(request))
+
+
+class NewProcessView(FGABrejaView):
+
+    def get(self, request):
+        return render_to_response('new_process.html', locals(),
                                   context_instance=RequestContext(request))
